39eb21f8338155af95b2787e5a9250a1ef3c7e87
CSNE <chansol98@gmail.com>
2018-05-01 17:55:14 +0900

    Added Position Marker.

--- coordinates.js
+++ coordinates.js
@@ -1,3 +1,5 @@
+//Basic coordinate arithmetic
+
 function degreesToRadians(d){
   return (d/360)*(2*Math.PI);
 }
@@ -9,35 +11,29 @@ function cosd(d){
   return Math.cos(degreesToRadians(d));
 }
 
-function Coordinates(){
-  this.lat=0;
-  this.long=0;
-  this.alt=0;
-}
 
-var center={"lat":37.5602,"long":126.9368,"alt":50};
-var latitude=center.lat;
+var cartesianCenterCoords={"lat":37.5602,"long":126.9368,"alt":50};
 
 var earthRadius=6400*1000;
 var earthCircumference=2*Math.PI*earthRadius;
 var metersPerDegreeLatitude=earthCircumference/360;
 
-var earthCircumferenceAtLatitude=2*Math.PI*earthCircumference*sind(latitude);
+var earthCircumferenceAtLatitude=2*Math.PI*earthCircumference*sind(cartesianCenterCoords.lat);
 var metersPerDegreeLongitude=earthCircumferenceAtLatitude/360;
 
 
 function coordsToCartesian(coords){
-  var y=(coords.lat-center.lat)*metersPerDegreeLatitude;
-  var x=(coords.long-center.long)*metersPerDegreeLongitude;
-  var z=coords.alt-center.alt;
-  
+  var y=(coords.lat-cartesianCenterCoords.lat)*metersPerDegreeLatitude;
+  var x=(coords.long-cartesianCenterCoords.long)*metersPerDegreeLongitude;
+  var z=coords.alt-cartesianCenterCoords.alt;
+
   return {"x":x,"y":y,"z":z};
 }
 
 function cartesianToCoords(coords){
-  var lat=coords.y/metersPerDegreeLatitude+center.lat;
-  var long=coords.x/metersPerDegreeLongitude+center.long;
-  var alt=coords.z+center.alt;
-  
+  var lat=coords.y/metersPerDegreeLatitude+cartesianCenterCoords.lat;
+  var long=coords.x/metersPerDegreeLongitude+cartesianCenterCoords.long;
+  var alt=coords.z+cartesianCenterCoords.alt;
+
   return {"lat":lat,"long":long,"alt":alt};
 }
--- logging.js
+++ logging.js
@@ -1,7 +1,14 @@
+//Very simple logging framework.
+
 var debugMsgBox=document.getElementById("debugMsgBox");
 
+function timestamp(){
+  var d=new Date();
+  return ""+d.getMinutes()+":"+d.getSeconds()+"."+d.getMilliseconds();
+}
 function print(level,message){
-  debugMsgBox.innerHTML+="<span class=\"level"+level+"\">"+message+"</span><br>";
+  
+  debugMsgBox.innerHTML="<span class=\"level"+level+"\">"+timestamp()+" | "+message+"</span><br>"+debugMsgBox.innerHTML;
 }
 function print_verbose(message){
   print(1,message);
