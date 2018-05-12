12a9f10c6776d9fff0830216e46138deab7437d6
CSNE <chansol98@gmail.com>
2018-05-12 19:34:29 +0900

    Added basic classes for representing geographic data/graphs.

---
+++ geodata.js
@@ -0,0 +1,20 @@
+var GraphDatabase=(function(){
+  var masterGraph=new Graph();
+
+  var shinchonStation=new Vertex(37.55537,126.93687,0);
+  shinchonStation.name="신촌역";
+  masterGraph.addVertex(shinchonStation);
+
+  var yonseiMainGate=new Vertex(37.56018,126.93689,0);
+  yonseiMainGate.name="연세대 정문";
+  masterGraph.addVertex(yonseiMainGate);
+
+
+  var shinchonStation_yonseiMainGate=new Edge(shinchonStation,yonseiMainGate);
+  shinchonStation_yonseiMainGate.timeRequired.walk=6;
+  masterGraph.addEdge(shinchonStation_yonseiMainGate);
+
+
+
+  return masterGraph;
+})();
---
+++ graphentities.js
@@ -0,0 +1,51 @@
+
+function Graph(){
+  this.edges=[];
+  this.vertices=[];
+
+  this.dijkstra=function(src,dest){
+
+
+  }
+
+  this.addEdge=function(e){
+    for(var i=0;i<this.edges.length;i++){
+      if (e.name===this.edges[i].name) throw "Duplicate edge?";
+    }
+    this.edges.push(e);
+
+  }
+
+  this.addVertex=function(v){
+    for(var i=0;i<this.vertices.length;i++){
+      if (v.name===this.vertices[i].name) throw "Duplicate vertex?";
+    }
+    this.vertices.push(v);
+  }
+}
+
+function Edge(v1,v2){
+  this.v1=v1;
+  this.v2=v2;
+
+  this.timeRequired={};
+  this.timeRequired.walk=0;
+  this.timeRequired.run=0;
+
+  this.distance=v1.coordinates.getDistance(v2.coordinates);
+
+  this.outside=true;
+
+  this.name="Unnamed edge";
+
+
+}
+
+function Vertex(lat,long,alt){
+
+  this.coordinates=new CoordinateConversions.CoordinateSystem(lat,long,alt);
+
+  this.edges=[];
+
+  this.name="Unnamed vertex";
+}
