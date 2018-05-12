9e1d57d70deac76d6374605f27a532409930a31d
CSNE <chansol98@gmail.com>
2018-05-12 15:32:45 +0900

    Added log level visibility buttons and improved time formatting.

--- logging.js
+++ logging.js
@@ -19,9 +19,35 @@
 var Log=(function(){
   var display=null;
 
+  var buttonsList=[];
+
+  var currentVisibilityOptions;
+
   function init(displayElement){
-    display=displayElement;
-    display.className+="loggingDisplay"
+
+    var buttonsContainer=displayElement.ownerDocument.createElement("div");
+    buttonsContainer.className+="loggingControls";
+    displayElement.appendChild(buttonsContainer);
+
+
+    for(var i=0;i<5;i++){
+      buttonsList[i]=displayElement.ownerDocument.createElement("input");
+      buttonsList[i].type="checkbox";
+      if (i>1) buttonsList[i].checked=true;
+      buttonsList[i].addEventListener("change",getVisibilityFromButtonsAndSetVisibility);
+      buttonsContainer.appendChild(buttonsList[i]);
+      var label=displayElement.ownerDocument.createElement("label");
+      label.innerHTML=["Verbose","Debug","Info","Warning","Error"][i];
+      buttonsContainer.appendChild(label);
+
+    }
+
+
+    display=displayElement.ownerDocument.createElement("p");
+    display.className+="loggingDisplay";
+    displayElement.appendChild(display);
+
+
     var css = displayElement.ownerDocument.createElement("style");
     css.type = "text/css";
     var cssText="";
@@ -31,13 +57,28 @@ var Log=(function(){
     cssText+= ".loggingDisplay .level4 {color:orange;}\n";
     cssText+= ".loggingDisplay .level5 {color:red;}\n";
     cssText+= ".loggingDisplay {overflow:scroll;}\n";
+    cssText+= ".loggingDisplay {font-family:consolas, monospace;}\n";
+    cssText+= ".loggingDisplay {display: flex;flex-flow: column;  height: 100%;}\n";
+    cssText+= ".loggingControls {padding:4px;}\n";
+    cssText+= ".loggingControls * {margin:4px;}\n";
     css.innerHTML=cssText;
     displayElement.ownerDocument.head.appendChild(css);
+
+    getVisibilityFromButtonsAndSetVisibility();
   }
 
   function timestamp(){
     var d=new Date();
-    return ""+d.getMinutes()+":"+d.getSeconds()+"."+d.getMilliseconds();
+    
+    var m=""+d.getMinutes();
+    var s=""+d.getSeconds();
+    var ms=""+d.getMilliseconds();
+
+    while (m.length<2) m="0"+m;
+    while (s.length<2) s="0"+s;
+    while (ms.length<3) ms="0"+ms;
+
+    return ""+m+":"+s+"."+ms;
   }
   function prependChild(elem){
     if (display!=null){
@@ -56,7 +97,10 @@ var Log=(function(){
       elem.innerHTML+=timestamp();
       elem.innerHTML+=" | ";
       elem.innerHTML+=message;
-      
+
+      if (currentVisibilityOptions[elem.classList[0]]) elem.style.display="inline";
+      else elem.style.display="none";
+
       elem.appendChild(display.ownerDocument.createElement("br"));
 
       prependChild(elem);
@@ -81,6 +125,16 @@ var Log=(function(){
     print(5,message);
   }
 
+  function getVisibilityFromButtonsAndSetVisibility(){
+    res={};
+    for (var i=0;i<5;i++){
+      res["level"+(i+1)]=buttonsList[i].checked;
+    }
+    currentVisibilityOptions=res;
+
+    setVisibility(currentVisibilityOptions);
+  }
+
   function setVisibility(options){
     if (display!=null){
       var children = display.childNodes;
@@ -89,7 +143,7 @@ var Log=(function(){
           if (options[children[i].classList[0]]) children[i].style.display="inline";
           else children[i].style.display="none";
         }
-        
+
       }
     }else{
       throw "No display element! Make sure init() is called with proper HTML element."
@@ -107,5 +161,3 @@ var Log=(function(){
     setVisibility:setVisibility
   }
 })()
-
-
