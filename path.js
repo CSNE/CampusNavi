41a5ac677d7f8815791e23a297645408ebf59b17
photon1592 <photon1592@gmail.com>
2018-05-24 08:28:06 +0900

    길이 없는 경우 처리, 뛰기, (날기) 추가, 이것저것
    generator에서 json을 parse하여 작업할 수 있음
    json attribute추가

--- path.js
+++ path.js
@@ -13,33 +13,44 @@ function Path(graph, from, to, pref)
     this.pref=pref;
 
     this.p = this.graph.findShortestPath(from, to, pref);
+    this.layers = [];
+    if (this.p) {
 
-    this.latlngs = [];
-    for (var i = this.p; i; i = i.p) {
-        var c = i.v.coordinates;
+        this.latlngs = [];
+        for (var i = this.p; i; i = i.p) {
+            var c = i.v.coordinates;
 
-        this.latlngs.push([c.lat, c.long]);
+            this.latlngs.push([c.lat, c.long]);
+        }
+
+        if (this.latlngs.length)
+            this.layers.push(L.polyline(this.latlngs, { "color": "red" }));
+
+        this.timeRequired = this.p.w;
+        Log.info("Found path from " + from.name + " to " + to.name + "(" + this.p.w.toFixed() + "초)");
+    }
+    else
+    {
+        this.timeRequired = Infinity;
+        this.latlngs = [[from.coordinates.lat, from.coordinates.long], [to.coordinates.lat, to.coordinates.long]];
+
     }
 
-    this.timeRequired=this.p.w;
-    Log.info("Found path from " + from.name + " to " + to.name + "(" + this.p.w.toFixed() + "초)");
+    for (var i = 0; i < this.latlngs.length; i++) {
+        this.layers.push(L.circleMarker(this.latlngs[i], { "radius": 4, "stroke": false, "fill": true, "fillColor": "blue", "fillOpacity": 1 }));
+    }
 
 
-
-    this.displayOnMap=function(map){
-        this.path_data = [];
-        if (this.latlngs.length)
-            this.path_data.push(L.polyline(this.latlngs, { "color": "red" }).addTo(map));
-        for (var i = 0; i < this.latlngs.length; i++) {
-            this.path_data.push(L.circleMarker(this.latlngs[i], { "radius": 4, "stroke": false, "fill": true, "fillColor": "blue", "fillOpacity": 1 }).addTo(map));
+    this.displayOnMap = function (map) {
+        for (var i = 0; i < this.layers.length; i++)
+        {
+            this.layers[i].addTo(map);
         }
     }
 
     this.removeFromMap = function(map) {
-        if (this.path_data) {
-            for (var i = 0; i < this.path_data.length; i++) {
-                map.removeLayer(this.path_data[i]);
-            }
+        for (var i = 0; i < this.layers.length; i++) {
+            this.layers[i].removeFrom(map);
         }
     }
 
