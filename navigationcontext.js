75c16f7535a152c4f781ff04c446db7c0ee8df78
CSNE <chansol98@gmail.com>
2018-05-26 03:41:15 +0900

    Arrival time estimation is now functional. When in navigation mode, the current location will be tracked and a estimated arrival time will be displayed on the page, updating as the user's location changes..

--- navigationcontext.js
+++ navigationcontext.js
@@ -3,10 +3,21 @@
 function NavigationContext(path){
     this.path=path;
 
-    this.totalTime=path.timeRequired;
-    this.currentLocationEstimatedTime=0;
-    this.timeMultiplier=1;
+    this.history=[];
+    this.historyThreshold=60*1000//milliseconds;
 
+    this.currentStatus;
+  
+    //Given two endpoints of a line startPointCoords and endPointCoords
+    //and another point pointCoords
+    //Finds the distance beween the line and the point
+    //Along with the perpendicular point
+    //And the "location ratio" of the perpendicular point within a line.
+    //    The location ratio is 0 when the point is directly over the startPoint,
+    //                          0.5 when the point is halfway over the line,
+    //                          and 1 when the point is directly over the endPoint
+    //All coordinates are given as a CoordinateSystem object.
+    //Z coords are ignored.
     function findDistanceBetweenLineAndPoint(startPointCoords,endPointCoords,pointCoords){
         /*
              S----------H----E
@@ -22,9 +33,6 @@ function NavigationContext(path){
         var vectorS=startPointCoords.to2DVector();
         var vectorE=endPointCoords.to2DVector();
         var vectorP=pointCoords.to2DVector();
-
-
-
 
         var vectorSE=vectorE.sub(vectorS);
         var vectorSP=vectorP.sub(vectorS);
@@ -44,7 +52,10 @@ function NavigationContext(path){
             "distance":distance
         };
     }
-
+    
+    //Given a Path object,
+    //And a point (a CoordinateSystem object)
+    //Finds the closest graph entity to the point.
     function findClosestEdgeInPath(path,point){
         var result=null;
         var currentMinValue=1000000; //1000km should be enough
@@ -83,71 +94,139 @@ function NavigationContext(path){
             if (currentDistance<currentMinValue) {
                 currentMinValue=currentDistance;
                 result={"type":"vertex",
-                    "vertex":currentVertex,
-                    "closestLocation":currentVertex.coordinates
-                   };
+                        "vertex":currentVertex,
+                        "closestLocation":currentVertex.coordinates
+                       };
             }            
         }
         return result;
     }
 
-
+    //Remove this variable later on.
     this.DEBUG_VARIABLE=null;
+    
+    //Update the location.
+    //location is given as a CoordinateSystem object.
+    //this method will also update the history, speed, predicted time of arrival,
+    //and basically everything.
+    //The calculated, useful values are stored in .currentStatus
+    //and is also available with a callback function (see .addContextUpdateListener())
     this.updateLocation=function(location){
         //We need to do a lot of shit here
         var result=findClosestEdgeInPath(path,location);
-        
         var totalTimeInitialEstimate=path.timeRequired;
-        var currentTimeInitialEstimate;
+        var estimatedTimeAtCurrentLocation;
+        var routeCompletionRatio;
+        var completionPerMillisecond;
 
         this.DEBUG_VARIABLE=result;
-        
+
         if (result.type==="edge"){
-            
+
             var pathToStartVertex= new Path(
-                    GraphDatabase,
-                    this.path.from,
-                    result.edge.vStart,
-                    this.path.pref);
+                GraphDatabase,
+                this.path.from,
+                result.edge.vStart,
+                this.path.pref);
             var timeToStartVertex=pathToStartVertex.timeRequired;
-            
+
             var pathToEndVertex= new Path(
-                    GraphDatabase,
-                    this.path.from,
-                    result.edge.vEnd,
-                    this.path.pref);
+                GraphDatabase,
+                this.path.from,
+                result.edge.vEnd,
+                this.path.pref);
             var timeToEndVertex=pathToEndVertex.timeRequired;
-            currentTimeInitialEstimate=timeToEndVertex*result.edgeCompletionRatio+
-                                       timeToStartVertex*(1-result.edgeCompletionRatio);
-            
-            Log.verbose("Navigation Context Updated.\n"+
+
+
+            estimatedTimeAtCurrentLocation=timeToEndVertex*result.edgeCompletionRatio+
+                timeToStartVertex*(1-result.edgeCompletionRatio);
+
+            Log.verbose("Projecting current position to graph...\n"+
                         "Closest graph element: edge\n"+
                         "Edge Start: "+result.edge.vStart.name+"\n"+
                         "Edge End: "+result.edge.vEnd.name+"\n"+
                         "Edge Ratio: "+result.edgeCompletionRatio+"\n"+
                         "Time to start vertex: "+timeToStartVertex+"\n"+
                         "Time to end vertex: "+timeToEndVertex+"\n"+
-                        "Time to current position: "+currentTimeInitialEstimate);
+                        "Time to current position: "+estimatedTimeAtCurrentLocation);
         }else if(result.type==="vertex"){
             var pathToVertex= new Path(
-                    GraphDatabase,
-                    this.path.from,
-                    result.vertex,
-                    this.path.pref);
+                GraphDatabase,
+                this.path.from,
+                result.vertex,
+                this.path.pref);
             var timeToVertex=pathToVertex.timeRequired;
-            
-            Log.verbose("Navigation Context Updated.\n"+
+
+
+            estimatedTimeAtCurrentLocation=timeToVertex;
+
+            Log.verbose("Projecting current position to graph...\n"+
                         "Closest graph element: vertex\n"+
                         "Vertex Name: "+result.vertex.name+"\n"+
-                        "Time to current position: "+timeToVertex);
+                        "Time to current position: "+timeToVertex);            
         }else{
             Log.error("what?");
         }
-        
-    }
-    this.destroy=function(){
 
-    }
+        routeCompletionRatio=estimatedTimeAtCurrentLocation/totalTimeInitialEstimate;
+        var currentTime=Date.now();
+        this.history.push({"timestamp":currentTime,
+                           "result":result,
+                           "estimatedTimeAtCurrentLocation":estimatedTimeAtCurrentLocation,
+                           "routeCompletionRatio":routeCompletionRatio
+                          });
+
+        while(this.history[0].timestamp<(currentTime-this.historyThreshold)){
+            this.history.shift();
+        }
         
+        Log.debug("History size "+this.history.length);
+
+
+        //TODO replace this with a more sophiscated algorithm.
+        if (this.history.length>=2){
+            var firstElem=this.history[0];
+            var lastElem=this.history[this.history.length-1];
+
+            completionPerMillisecond=(lastElem.routeCompletionRatio-firstElem.routeCompletionRatio)/(lastElem.timestamp-firstElem.timestamp);
+        }else{
+            completionPerMillisecond=NaN;
+        }
+        
+        var ratioLeft=1-routeCompletionRatio;
+        var timeLeft=ratioLeft/completionPerMillisecond; //in milliseconds
+        var arrivalTime=currentTime+timeLeft;
+
+        
+        this.currentStatus={"routeCompletionRatio":routeCompletionRatio,
+                            "completionPerMillisecond":completionPerMillisecond,
+                            "timeLeft":timeLeft,
+                            "arrivalTime":arrivalTime,
+                            "updated":currentTime};
+        
+        Log.verbose("Navigation Context updated.\n"+
+                    "Completion Ratio: "+routeCompletionRatio+"\n"+
+                    "Time left: "+(timeLeft/1000).toFixed()+" seconds.\n"+
+                    "Ratio per minute: "+(completionPerMillisecond/1000*100).toPrecision(5)+"(percent per minute)"
+                   );
+        
+        this.callCallbacks(this.currentStatus);
+    }
+    
+    this.callbacks=[]
+    
+    //Add a callback.
+    //the callback will be called
+    //whenever the .currentStatus is modified
+    //which is whenever .updateLocation() is called.
+    this.addContextUpdateListener=function(callback){
+        this.callbacks.push(callback);
+    }
+    this.callCallbacks=function(data){
+        for(var i=0;i<this.callbacks.length;i++){
+            this.callbacks[i](data);
+        }
+    }
+    
     Log.debug("Navigation Context Created.\nTarget path is "+this.path.from.name+" >> "+this.path.to.name);
 }
