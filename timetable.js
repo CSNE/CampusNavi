017be1fd59a4b3cb1d7b15a9164e3998cfb9e913
oheat87 <39119468+oheat87@users.noreply.github.com>
2018-06-04 21:31:07 +0900

    vertex 문자열 호환 및 메소드 몇개

--- timetable.js
+++ timetable.js
@@ -83,38 +83,47 @@ function TimeTable(){
 	
 	this.lsUsable = (typeof(localStorage)=="undefined")? false : true;
 	
-	this.buildings = ['공학원', '제1공학관', '제2공학관', '제3공학관', '제4공학관', '간호대학', '의과대학', 
+	this.buildings = ['공학원', '제1공학관', '제2공학관', '제3공학관', '제4공학관', '대강당', 
 						'경영관', '음악관', '체육관', '체육교육관', '백양관', '과학관', '과학원', '대우관',
-						'외솔관', '외솔관2', '교육과학관', '위당관', '삼성관', '신학관', '빌링슬리관', '연희관',
-						'성암관', '상남경영관'];
-	this.classPrefix = ['외', '신', '백', '교', '위', '위B', '상본', '상본B', '경영', '경영B',
+						'외솔관', '교육과학관', '위당관', '삼성관', '신학관', '빌링슬리관', '연희관',
+						'성암관', '상남경영관', '광복관' ,'스포츠과학관', '아펜젤러관'];
+	/*this.classPrefix = ['외', '신', '백S', '교', '위', '위B', '상본', '상본B', '경영', '경영B',
 						'과', '과S', '과B', '공A', '공B', '공C', '공D', '신', '빌', '연', '성',
-						'음', '삼', '삼B', '스포츠', '체'];
+						'음(A)', '음(B)', '삼', '삼B', '스포츠', '체', '광', '아'];*/
+	this.buildingPrefix = {'외':"외솔관", '신':"신학관", '백S':"백양관", '교':"교육과학관", '위':"위당관", '위B':"위당관",
+						'과':"과학관",  '과B':"과학관", '공A':"제1공학관", '공D':"제4공학관", '신':"신학관", '빌':"빌링슬리관",
+						'연':"연희관", '성':"성암관", '음A':"음악관A", '음B':"음악관B", '경영':"경영관", '경영B':"경영관",
+						'음(A)':"음악관A", '음(B)':"음악관B", '광':"광복관", '아':"아펜젤러관",
+						'삼':"정문", '삼B':"정문", '상본':"정문", '상본B':"정문",   //
+						'공B':"정문", '공C':"정문", //
+						'과S':"과학원", '스포츠':"스포츠과학관", '체':"체육교육관"};					
 	
 	this.locationCheck = function(location) {
 	/*
 		입력받은 강의실 정보가 유효하면 0반환, 아니면 0이 아닌 값 반환
 	*/
 		var loc = location;
-		while (loc.charAt(0) == ' ') {
+		/*while (loc.charAt(0) == ' ') {
             loc = loc.substring(1);
-        }
-		for(var i=0; i<this.buildings.length; i++) {
+        }*/
+		var i;
+		for(i=0; i<this.buildings.length; i++) {
 			if(loc == this.buildings[i]) return 0;
 		}
-		for(var i=0; i<loc.length; i++) {
+		for(i=0; i<loc.length; i++) {
 			if(loc.charAt(i)>='0' && loc.charAt(i)<='9') break;
 		}
 		var prefix = loc.substring(0,i);
 		var suffix = loc.substring(i,loc.length);
 		if(i>=loc.length || suffix.length!=3) return 1;
 		
-		for(var i=0; i<this.classPrefix.length; i++) {
+		/*for(var i=0; i<this.classPrefix.length; i++) {
 			if(prefix == this.classPrefix[i]) break;
 		}
-		if(i>=this.classPrefix.length) return 2;
+		if(i>=this.classPrefix.length) return 2;*/
+		if(!this.buildingPrefix.hasOwnProperty(prefix)) return 2;
 		
-		for(var i=0; i<suffix.length; i++) {
+		for(i=0; i<suffix.length; i++) {
 			if(suffix.charAt(i)<'0' || suffix.charAt(i)>'9') return 3;
 		}
 		
@@ -299,7 +308,8 @@ function TimeTable(){
     }
     this.lastLecture=function(){
     /*
-        방금 끝난 수업을 반환함.
+        방금 끝난 수업을 반환함.현재 시각 기준 끝난 수업 가운데 가장 최근에 끝난 수업 반환(tte)
+		아직 끝난 수업이 없거나 오늘 수업이 없는 경우 null 반환
     */
 		var date = new Date(); // 날짜 및 시간
         if (this.debug_override_time) date=this.debug_override_time; //for debug & demo
@@ -319,6 +329,47 @@ function TimeTable(){
 		return justClass;
 
     }
+	
+	this.nextLocation = function() {
+	/*
+        곧 시작하는 수업의 강의 건물 문자열 반환함(ex "제1공학관")
+		곧 시작하는 수업이 없으면 "정문"문자열 반환함
+    */
+		var soonClass = this.nextLecture();
+		if(soonClass==null) return "정문";
+		
+		var loc = soonClass.location;
+		var i;
+		for(i=0; i<this.buildings.length; i++) { // 강의실이 건물 이름으로 등록된 경우
+			if(loc == this.buildings[i]) return loc;
+		}
+
+		for(i=0; i<loc.length; i++) {	// 강의실이 정식 약어로 등록된 경우
+			if(loc.charAt(i)>='0' && loc.charAt(i)<='9') break;
+		}
+		return this.buildingPrefix[loc.substring(0,i)];
+		
+	}
+	
+	this.lastLocation = function() {
+	/*
+        가장 최근에 끝난 수업의 강의 건물 문자열 반환함(ex "제1공학관")
+		가장 최근에 끝난 수업이 없으면 "정문"문자열 반환함
+    */
+		var justClass = this.lastLecture();
+		if(justClass==null) return "정문";
+		
+		var loc = justClass.location;
+		var i;
+		for(i=0; i<this.buildings.length; i++) { // 강의실이 건물 이름으로 등록된 경우
+			if(loc == this.buildings[i]) return loc;
+		}
+
+		for(i=0; i<loc.length; i++) {	// 강의실이 정식 약어로 등록된 경우
+			if(loc.charAt(i)>='0' && loc.charAt(i)<='9') break;
+		}
+		return this.buildingPrefix[loc.substring(0,i)];
+	}
 	
 	this.saveTimeTable=function() {
 	/*
